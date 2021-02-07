abf2d642fa8ad27992a6593fe18f24b599645153
Lakshay Bhatia <lakshaybhatia999@gmail.com>
2021-02-07 23:46:18 +1100

    Initial Commit

---
+++ Github Logo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Github Logo
+//
+//  Created by Lakshay Bhatia on 7/2/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
